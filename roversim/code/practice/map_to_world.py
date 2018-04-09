2952c14ec784806b3640b8d9b1f2b3750c2ca397
Anupam Kaul <anupam.kaul@yahoo.com>
2018-04-09 16:05:46 -0700

    Finished world map coordinates mapping
    1. Add rotation, translation and clipping features
    2. The 'place' where the rover is actually, and the yaw angle, are both chosen at random
    (which is why on the world map, the 'rover' can appear almost anywhere)
    3. LGTM

--- roversim/code/practice/map_to_world.py
+++ roversim/code/practice/map_to_world.py
@@ -74,9 +74,6 @@ plt.xlim(0, 160)
 plt.title('Rover-Centric Map', fontsize=20)
 plt.show() 
 
-# map to world coordinates now
-# we will assume a 'random' yaw angle for now  (see rover_vs_world.png)
-
 '''
 Notes from the udacity pages:
 
@@ -112,6 +109,97 @@ x_pix_world = np.clip(x_world, 0, world_size - 1)
 y_pix_world = np.clip(y_world, 0, world_size - 1)
 
 '''
+
+# map to world coordinates now
+# we will assume a 'random' yaw angle for now  (see rover_vs_world.png)
+
+rover_yaw = np.random.random(1)*360
+print (rover_yaw)
+
+# generate random rover positions in world coordinates (where the rover may be at any given point of time)
+# Position values will range from 20 to 180 to avoid 
+# the edges in a 200 * 200 pixel world.
+
+rover_xpos = np.random.random(1)*160 + 20
+rover_ypos = np.random.random(1)*160 + 20
+
+# note that since we have now chosen random values for yaw and positions,
+# multiple runs of the code will result in different outputs each time.
+
+# define the rotation function
+def rotate_pix(xpix, ypix, yaw):
+	
+	# convert yaw angle to radians
+	yaw_rad = yaw * np.pi / 180
+
+	xpix_rotated = (xpix*np.cos(yaw_rad) - ypix*np.sin(yaw_rad))
+	ypix_rotated = (ypix*np.cos(yaw_rad) + xpix*np.sin(yaw_rad))
+
+	return xpix_rotated, ypix_rotated
+
+
+#define the translation function
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale):
+
+	# apply scaling and translation
+	xpix_translated = (xpix_rot / scale) + xpos
+	ypix_translated = (ypix_rot / scale) + ypos
+
+
+	return xpix_translated, ypix_translated
+
+
+# define the pipeline function to apply rotation and translation (and clipping)
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+
+	# apply rotation
+	xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+
+	# apply translation
+	xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+
+	# clip to world size
+	x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size)
+	y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size)
+
+	return x_pix_world, y_pix_world
+
+# Apply this code now ..
+# Generate a 200 * 200 pixel worldmap
+worldmap = np.zeros((200, 200))
+scale = 10
+
+# get navigable pixel positions in world coords
+# (where xpix, ypix are rover coordinates from the function rover_coords())
+
+x_world, y_world = pix_to_world(xpix, ypix, rover_xpos, rover_ypos, rover_yaw, worldmap.shape[0], scale)
+
+# Add pixel positions to worldmap
+worldmap[y_world, x_world] += 1
+print('Xpos rover = ', rover_xpos, 'Ypos rover= ', rover_ypos, 'Yaw = ', rover_yaw)
+print('Xworld = ', x_world, 'Yworld= ', y_world) # x_world and y_world are arrays
+
+
+# Do the showing part now ...
+
+# Plot the map in rover-centric coordinates
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 7))
+f.tight_layout()
+ax1.plot(xpix, ypix, '.')
+ax1.set_title('Rover Space', fontsize=40)
+ax1.set_ylim(-160, 160)
+ax1.set_xlim(0, 160)
+ax1.tick_params(labelsize=20)
+
+ax2.imshow(worldmap, cmap='gray')
+ax2.set_title('World Space', fontsize=40)
+ax2.set_ylim(0, 200)
+ax2.tick_params(labelsize=20)
+ax2.set_xlim(0, 200)
+
+plt.subplots_adjust(left=0.1, right=1, top=0.9, bottom=0.1)
+plt.show() 
 
 
 
