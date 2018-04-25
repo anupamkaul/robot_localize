6dc70b384225ab5a5690023da427a34bc5832034
Anupam Kaul <anupam.kaul@yahoo.com>
2018-04-25 12:10:13 -0700

    Add the send_control function

--- roversim/code/practice/rover_state.py
+++ roversim/code/practice/rover_state.py
@@ -67,4 +67,24 @@ steering angle and take action!
 
 '''
 
+# Sending commands to the Rover
+# ----------------------------
+
+def send_control(commands, image_string1, image_string2):
+
+	# Define commands to be sent to the rover
+	data = {
+		'throttle' : commands[0].__str__(),
+		'brake': commands[1].__str__(),
+		'steering_angle': commands[2].__str__(),
+		'inset_image1': image_string1,
+		'inset_image2': image_string2, 
+	}
+
+	# Send commands via socketIO server
+	sio.emit(
+		"data",
+		data,
+		skip_sid = True
+	)
 
