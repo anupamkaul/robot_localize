a62989174650e12b4821fe90a76afddfff4b1294
root <anupam.kaul@yahoo.com>
2019-02-05 03:41:05 +0000

    Use Gazebo to create first robot model

---
+++ myrobot/script/hello.cpp
@@ -0,0 +1,21 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+    class WorldPluginMyRobot : public WorldPlugin
+    {
+
+        public WorldPluginMyRobot : WorldPlugin()
+        {
+            printf("Hello Robot World!\n")
+        }
+
+        public void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+        {
+
+        }
+
+    };
+
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
