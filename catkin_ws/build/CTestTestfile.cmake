6401a7068c1bbad6299089bae1d580ffcce378cf
Anupam Kaul <anupam.kaul@yahoo.com>
2019-02-22 14:18:43 -0800

    Found workaround for roslaunch not working
    Replaced gazebo 9 with gazebo 7 and roslaunch works now.
    Trying out simple arm.

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/anupam/github/robond/catkin_ws/src
+# Build directory: /home/anupam/github/robond/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(simple_arm)
