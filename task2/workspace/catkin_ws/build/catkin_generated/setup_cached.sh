317d78130272d2a46a3d6a8ab684ea25a04c1ed6
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-10 07:52:44 -0700

    Fix laptop issues with robot arm simple_move, update notes

--- task2/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
+++ task2/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/anupam/github/robond/task2/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/anupam/github/robond/task2/workspace/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/anupam/github/robond/task2/workspace/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/anupam/github/robond/task2/workspace/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/anupam/github/robond/task2/workspace/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/anupam/github/robond/task2/workspace/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/anupam/github/robond/task2/workspace/catkin_ws/src:$ROS_PACKAGE_PATH"
--- workspace/catkin_ws/build/catkin_generated/setup_cached.sh
+++ workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH="/home/anupam/github/robond/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/home/anupam/github/robond/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/anupam/github/robond/workspace/catkin_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/home/anupam/github/robond/workspace/catkin_ws/devel/lib/python3/dist-packages"
