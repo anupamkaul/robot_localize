5faabe337f20e9db644ba87ec9f8dc292f8728fb
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-22 17:55:37 -0700

    Add state prediction step for kalman filter

--- practice/localization/kalman/k1.cpp
+++ practice/localization/kalman/k1.cpp
@@ -1,5 +1,6 @@
-// 2 Gaussians w.r.t measurements (of position) - one represents the prior belief, and the second represents the actual measurements from sensors.
+// The Measurement Update step:
 
+// 2 Gaussians w.r.t measurements (of position) - one represents the prior belief, and the second represents the actual measurements from sensors.
 // Since both have uncertainties, hence both are represented by unimodal gaussian distributions. From these 2 gaussians we calculate the posterior gaussian.
 
 // The posterior belief gaussian gets a more confident value because it takes the gaussian-distributed uncertainties (variances and mean) into account using the formulas below.
@@ -14,6 +15,17 @@
 // This is what happens during the Measurement Update part of the Kalman Filter
 
 // ----------------------------------------------------------------------------
+
+// State prediction takes places after an inevitably uncertain motion (control). 
+// Since measurement and prediction are iterative cycles in the kalman filter, so the 
+// Posterior gaussian from measurement step is now considered as the prior belief of the state (position) for the 
+// state prediction step. (i.e, the robot's best estimate of its current location)..
+
+// The robot now executes a command (control step0 - move forward by xyz meters. The resultant gaussian is centered around xyz as mean with a variance abc.
+// Calculating the state estimate is very simple:
+
+// new mean = prior's mean + motion's mean
+// new variance = prior's variance + motion's variance
 
 #include <iostream>
 #include <math.h>
@@ -34,9 +46,21 @@ tuple<double, double> measurement_update(double mean1, double var1, double mean2
     return make_tuple(new_mean, new_var);
 }
 
+tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2)
+{
+    new_mean = mean1 + mean2;
+    new_var =  var1 + var2;
+    return make_tuple(new_mean, new_var);
+}
+
+
 int main()
 {
    tie(new_mean, new_var) = measurement_update(10, 8, 13, 2);
    printf("[%f, %f]\n", new_mean, new_var);
+
+   tie(new_mean, new_var) = state_prediction(10, 4, 12, 4);
+   printf("[%f, %f]\n", new_mean, new_var);
+
    return 0;
 }
