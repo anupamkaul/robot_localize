76c6d90d8be69a0304491af73be0c2b94c6c8367
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-26 12:33:00 -0700

    Add basline pkg for EKF based sensor fusion

---
+++ samples/ekf_sensor_fusion/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
+export PWD="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src:/opt/ros/kinetic/share"
