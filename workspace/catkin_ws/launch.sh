98ecbf4b93f27a2533c1746d909ed4b458a3cc33
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-26 10:49:23 -0700

    Add launch file for simple arm robot

---
+++ workspace/catkin_ws/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+xterm -e 'source devel/setup.bash ; roslaunch simple_arm robot_spawn.launch' & 
+sleep 10 
+
+xterm -e 'source devel/setup.bash ; rqt_image_view /rgb_camera/image_raw' & 
+sleep 5
+
+xterm -hold -e 'source devel/setup.bash ; rosservice call /arm_mover/safe_move "joint_1: 0 
+joint_2: 0"'
+
+
+
+
+
+
