38bff13f0fa207e70b7d4e87955007abfd978fbf
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-26 13:15:56 -0700

    Add turlebot 2 (kobuki) simulation package
    compiles and launches

--- samples/ekf_sensor_fusion/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
+++ samples/ekf_sensor_fusion/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -5,12 +5,24 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export TURTLEBOT_3D_SENSOR="asus_xtion_pro"
+export TURTLEBOT_BASE="kobuki"
+export TURTLEBOT_BATTERY="/sys/class/power_supply/BAT0"
+export TURTLEBOT_GAZEBO_MAP_FILE="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/maps/playground.yaml"
+export TURTLEBOT_GAZEBO_WORLD_FILE="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world"
+export TURTLEBOT_INTERACTIONS_LIST="[turtlebot_bringup/admin.interactions, turtlebot_bringup/documentation.interactions, turtlebot_bringup/pairing.interactions, turtlebot_bringup/visualisation.interactions]"
+export TURTLEBOT_MAP_FILE="/opt/ros/kinetic/share/turtlebot_navigation/maps/willow-2010-02-18-0.10.yaml"
+export TURTLEBOT_NAME="turtlebot"
+export TURTLEBOT_RAPP_PACKAGE_BLACKLIST="[]"
+export TURTLEBOT_RAPP_PACKAGE_WHITELIST="[rocon_apps, turtlebot_rapps]"
+export TURTLEBOT_SERIAL_PORT="/dev/kobuki"
+export TURTLEBOT_SIMULATION="false"
+export TURTLEBOT_STACKS="hexagons"
+export TURTLEBOT_STAGE_MAP_FILE="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/maze.yaml"
+export TURTLEBOT_STAGE_WORLD_FILE="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/stage/maze.world"
+export TURTLEBOT_STDR_MAP_FILE="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stdr/maps/sparse_obstacles.yaml"
+export TURTLEBOT_TYPE="turtlebot"
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/devel:/opt/ros/kinetic"
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
+export PKG_CONFIG_PATH="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build"
-export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/src:/opt/ros/kinetic/share"
--- samples/ekf_sensor_fusion/workspace/catkin_ws/build/cmake_install.cmake
+++ samples/ekf_sensor_fusion/workspace/catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,10 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build/turtlebot_simulator/turtlebot_gazebo/cmake_install.cmake")
+  include("/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build/turtlebot_simulator/turtlebot_simulator/cmake_install.cmake")
+  include("/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build/turtlebot_simulator/turtlebot_stage/cmake_install.cmake")
+  include("/home/anupam/robond/samples/ekf_sensor_fusion/workspace/catkin_ws/build/turtlebot_simulator/turtlebot_stdr/cmake_install.cmake")
 
 endif()
 
