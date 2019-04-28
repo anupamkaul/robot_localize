6556b3b09f1282940120a128cb8d10193f4650e6
Anupam Kaul <anupam.kaul@yahoo.com>
2019-04-28 11:34:34 -0700

    Build, launch and connect the 2 nodes (turtlebot and robot_pose_ekf)
    * Disable vo topic's 3D pose, remap IMU subscrip to match turtle-bot (launch file)
    * Compile and build package
    * Update all notes in the notes file
    * Update main launch file of package
    * See how the topics connect at runtime by using rosgraph

--- samples/ekf_sensor_fusion/workspace/catkin_ws/launch.sh
+++ samples/ekf_sensor_fusion/workspace/catkin_ws/launch.sh
@@ -2,3 +2,11 @@
 
 xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 10
+
+xterm -e "source devel/setup.bash; roslaunch robot_pose_ekf robot_pose_ekf.launch" &
+sleep 5
+
+xterm -e "source devel/setup.bash; rosrun rqt_graph rqt_graph" &
+
+
+
