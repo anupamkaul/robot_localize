3ef570c483c4a78214576c09df2656e2ff6bd966
Anupam Kaul <anupam.kaul@yahoo.com>
2019-05-08 13:30:54 -0700

    Create simulation env (world and robot) for AMCL project

---
+++ task3/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export TURTLEBOT_3D_SENSOR="asus_xtion_pro"
+export TURTLEBOT_BASE="kobuki"
+export TURTLEBOT_BATTERY="/sys/class/power_supply/BAT0"
+export TURTLEBOT_INTERACTIONS_LIST="[turtlebot_bringup/admin.interactions, turtlebot_bringup/documentation.interactions, turtlebot_bringup/pairing.interactions, turtlebot_bringup/visualisation.interactions]"
+export TURTLEBOT_MAP_FILE="/opt/ros/kinetic/share/turtlebot_navigation/maps/willow-2010-02-18-0.10.yaml"
+export TURTLEBOT_NAME="turtlebot"
+export TURTLEBOT_RAPP_PACKAGE_BLACKLIST="[]"
+export TURTLEBOT_RAPP_PACKAGE_WHITELIST="[rocon_apps, turtlebot_rapps]"
+export TURTLEBOT_SERIAL_PORT="/dev/kobuki"
+export TURTLEBOT_SIMULATION="false"
+export TURTLEBOT_STACKS="hexagons"
+export TURTLEBOT_TYPE="turtlebot"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/anupam/robond/task3/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/anupam/robond/task3/workspace/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/anupam/robond/task3/workspace/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/anupam/robond/task3/workspace/catkin_ws/src:/home/anupam/robond/samples/simple_arm/workspace/catkin_ws/src:/opt/ros/kinetic/share"
