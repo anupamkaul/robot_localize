b3e838d7b397d3beb243ca34b962b9700e3368b8
Anupam Kaul <anupam.kaul@yahoo.com>
2019-05-09 18:27:47 -0700

    Modify launch file to include amcl launcher
    Even with both robot + amcl, I still see the same error
    (timeouts waiting for laser scans etc).
    
    Needs to be debugged. Probably rviz may not have been setup correctly
    or some parameters around timeouts may need to be increased.

--- task3/workspace/catkin_ws/launch.sh
+++ task3/workspace/catkin_ws/launch.sh
@@ -4,18 +4,25 @@
 # a -hold option can be used to keep xterm persistent upon an exception (before -e) 
 
 # House the robot into the world
-#xterm -e "source devel/setup.bash; roslaunch my_robot world.launch" & 
-#sleep 10 
+xterm -e "source devel/setup.bash; roslaunch my_robot world.launch" & 
+sleep 10 
+
+# -- map creation is one time only, so commenting the stages
+# note that map generation does not need the simulation in gazebo to be running
 
 # Create a map of the environment: (run map generation server)
-xterm -hold -e "source devel/setup.bash; gzserver src/pgm_map_creator/world/anupam.world" &
-sleep 5
+#xterm -hold -e "source devel/setup.bash; gzserver src/pgm_map_creator/world/anupam.world" &
+#sleep 5
 
 # Create a map of the environment: (run map client)
-xterm -e "source devel/setup.bash; roslaunch pgm_map_creator request_publisher.launch" &
+#xterm -e "source devel/setup.bash; roslaunch pgm_map_creator request_publisher.launch" &
+#sleep 5
+
+xterm -e "source devel/setup.bash; roslaunch my_robot amcl.launch" & 
 sleep 5
 
-# note that map generation does not need the simulation in gazebo to be running
+
+
 
 
 
