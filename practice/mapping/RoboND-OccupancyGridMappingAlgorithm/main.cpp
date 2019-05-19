541cb9a9f92f2ae69ea11e80d48848ef1a10bd7c
Anupam Kaul <anupam.kaul@yahoo.com>
2019-05-19 14:10:20 -0700

    Code the mapping visualization for OcGrMap
    Note that when I use white for unknown cells I can see
    cases where free and unknown co-incide, indicating that the
    mapping algorithm could be fine tuned further.

--- practice/mapping/RoboND-OccupancyGridMappingAlgorithm/main.cpp
+++ practice/mapping/RoboND-OccupancyGridMappingAlgorithm/main.cpp
@@ -78,12 +78,35 @@ void occupancyGridMapping(double Robotx, double Roboty, double Robottheta, doubl
 
 void visualization()
 {
-    //TODO: Initialize a plot named Map of size 300x150
-    
-    //TODO: Loop over the log odds values of the cells and plot each cell state. 
+    //Graph Format
+
+    //Initialize a plot named Map of size 300x150
+    plt::title("Map");
+    plt::xlim(0, (int)(mapWidth / gridWidth));
+    plt::ylim(0, (int)(mapHeight / gridHeight));
+
+    //Loop over the log odds values of the cells and plot each cell state. 
     //Unkown state: green color, occupied state: black color, and free state: red color 
-    
-    //TODO: Save the image and close the plot 
+
+    // Draw every grid of the map:
+    for (double x = 0; x < mapWidth / gridWidth; x++) {
+        cout << "Remaining Rows= " << mapWidth / gridWidth - x << endl;
+        for (double y = 0; y < mapHeight / gridHeight; y++) {
+            if (l[x][y] == l0) { //White unkown state
+                plt::plot({ x }, { y }, "w.");
+            }
+            else if (l[x][y] > locc) { //Black occupied state
+                plt::plot({ x }, { y }, "k.");
+            }
+            else { //Green free state (lfree)
+                plt::plot({ x }, { y }, "g.");
+            }
+        }
+    }
+
+    //Save the image and close the plot
+    plt::save("./Images/Map.png");
+    plt::clf();
 }
 
 int main()
