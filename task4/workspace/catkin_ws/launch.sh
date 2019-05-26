f027a92e273fd6b1dc13aa7d5e1e5acd7c7a24c3
Anupam Kaul <anupam.kaul@yahoo.com>
2019-05-26 13:04:31 -0700

    Play with rtabmap-databaseViewer
    Nothing shows up in the constraints and graph windows yet,
    probably because rtabmapViewer also shows nothing yet.
    (Need to look at all those warning messages from rtabmap node)
    
    Tried hooking up amcl with mapping as well ... see random duplicate
    map updates happening on rviz, but doesn't really move the needle w.r.t
    the warnings that rtabmap node's console is throwing.
    
    Updated launch file to work in sequence as suggested by the course.

--- task4/workspace/catkin_ws/launch.sh
+++ task4/workspace/catkin_ws/launch.sh
@@ -7,26 +7,32 @@
 xterm -e "source devel/setup.bash; roslaunch my_robot world.launch" & 
 sleep 10 
 
-# -- map creation is one time only, so commenting the stages
-# note that map generation does not need the simulation in gazebo to be running
-
-# Create a map of the environment: (run map generation server)
-#xterm -hold -e "source devel/setup.bash; gzserver src/pgm_map_creator/world/anupam.world" &
-#sleep 5
-
-# Create a map of the environment: (run map client)
-#xterm -e "source devel/setup.bash; roslaunch pgm_map_creator request_publisher.launch" &
-#sleep 5
-
-#xterm -e "source devel/setup.bash; roslaunch my_robot amcl.launch" & 
-xterm -e "source devel/setup.bash; roslaunch my_robot mapping.launch" & 
-sleep 5
-
 # Launch the optional teleop keyboard package to move the robot via keyboard 
 # instead of via nav move_base in rviz..
 
 xterm -e "source devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
 sleep 5
+
+# -- map creation is one time only, so commenting the stages
+# note that map generation does not need the simulation in gazebo to be running
+
+# Create a map of the environment: (run map generation server)
+
+#xterm -hold -e "source devel/setup.bash; gzserver src/pgm_map_creator/world/anupam.world" &
+#sleep 5
+
+# Create a map of the environment: (run map client)
+
+#xterm -e "source devel/setup.bash; roslaunch pgm_map_creator request_publisher.launch" &
+#sleep 5
+
+#xterm -e "source devel/setup.bash; roslaunch my_robot amcl.launch" & 
+#sleep 5
+
+xterm -e "source devel/setup.bash; roslaunch my_robot mapping.launch" & 
+sleep 5
+
+# rtabmap-databaseViewer ~/.ros/rtabmap.db
 
 
 
