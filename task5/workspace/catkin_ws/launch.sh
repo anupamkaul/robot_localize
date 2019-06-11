588e9148ad7c48d71f52dd92111a9ce5e9ec6403
Anupam Kaul <anupam.kaul@yahoo.com>
2019-06-11 00:15:01 -0700

    Add gmapping and rviz to task5
    Launch.sh tests that gmapping works in rviz.

--- task5/workspace/catkin_ws/launch.sh
+++ task5/workspace/catkin_ws/launch.sh
@@ -4,13 +4,20 @@
 # a -hold option can be used to keep xterm persistent upon an exception (before -e) 
 
 # House the robot into the world
-xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+#xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+xterm -e "source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/willowgarage.world" &
 sleep 10 
 
 # Launch the optional teleop keyboard package to move the robot via keyboard 
 # instead of via nav move_base in rviz..
 
 xterm -e "source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
+sleep 5
+
+xterm -e "source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
+sleep 5
+
+xterm -e "source devel/setup.bash ; rosrun rviz rviz -d src/rvizConfig/myslamconfig.rviz" &
 sleep 5
 
 #xterm -e "source devel/setup.bash; roslaunch my_robot mapping.launch" &
