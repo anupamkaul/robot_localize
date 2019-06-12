6a2a29d633ca39e2a501aa4c6d7f31364dd5416d
Anupam Kaul <anupam.kaul@yahoo.com>
2019-06-12 11:27:53 -0700

    Pass actual map of my_world in test_navigation.
    This is because amcl will compare the laser scan to the actual map
    and then localize. I also see that after some unsuccessful paths
    (mostly because of the structure of the map - its not got any doors
    or windows yet), if the end goal is within the same room and amcl/move_base
    thinks robot is free to move, it will move it to its eventual goal
    
    The screenshot attached shows how this works.

--- task5/workspace/catkin_ws/src/scripts/test_navigation.sh
+++ task5/workspace/catkin_ws/src/scripts/test_navigation.sh
@@ -11,11 +11,14 @@ xterm -e "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo t
 
 sleep 10 
 
+# make sure teleop is not running when move_base is on
 #xterm -e "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
 #sleep 5
 
-xterm -e "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+xterm -e "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/anupam/robond/task5/workspace/catkin_ws/src/my_robot/maps/map.yaml" & 
+
 #xterm -e "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo amcl.launch" & 
+
 sleep 5
 
 xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun rviz rviz -d src/rvizConfig/myslamconfig.rviz" &
