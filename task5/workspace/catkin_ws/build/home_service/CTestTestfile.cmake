bc47380e84a7384dfea1c70692473d3635ad2b46
Anupam Kaul <anupam.kaul@yahoo.com>
2019-06-13 01:17:53 -0700

    Add a home service node
    The home service node creates a publisher node to display / manipulate the markers,
    subscribes to odometry data to keep track of pickup and drop-off zones and takes
    subsequent actions.
    
    Tested via final homeservice script that everything is integrated and automated.

---
+++ task5/workspace/catkin_ws/build/home_service/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anupam/robond/task5/workspace/catkin_ws/src/home_service
+# Build directory: /home/anupam/robond/task5/workspace/catkin_ws/build/home_service
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
